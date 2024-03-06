82a42f0be534b8e035717751a204185cb89dd435
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-03-06 11:32:52 -0800

    1st Assignment
    ✔👍My  1st  "Typescript" Assignment DONE.... 
    https://www.linkedin.com/feed/update/urn:li:activity:7171070716350558208?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7171070716350558208%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3Bhma%2FHmFlSVaHsyNFfTDGSw%3D%3D

---
+++ main.ts
@@ -0,0 +1,8 @@
+/* 
+store a person's name in a variable, and peint a message to that person. your message should be simple, 
+such as, "Hello Eric, would you like to learn some python today?" 
+*/
+
+
+let personName: string = "Maham shahid ";
+console.log("Hello" + personName +", would you like to learn some python today?");
